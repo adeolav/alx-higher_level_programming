423e94aa8e36c9ac42feee9b5a1e82f6a5fb0cfe
Adewunmi Oladele <rkutalian@gmail.com>
2023-03-31 08:29:35 +0100

    Update 4-hbtn_status.py

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-"""fetches https://intranet.hbtn.io/status."""
+"""fetches https://alx-intranet.hbtn.io/status."""
 import requests
 
 
 if __name__ == "__main__":
-    r = requests.get("https://intranet.hbtn.io/status")
+    r = requests.get("https://alx-intranet.hbtn.io/status")
     print("Body response:")
     print("\t- type: {}".format(type(r.text)))
     print("\t- content: {}".format(r.text))
